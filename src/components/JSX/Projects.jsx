2e5cb4fb922d434bc8a748d8cf7861701a231e71
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-02-05 16:16:53 +0100

    Adjusted positioning

--- src/components/JSX/Projects.jsx
+++ src/components/JSX/Projects.jsx
@@ -1,6 +1,7 @@
 import LH from "../img/learnhall.png";
 import SW from "../img/switch-nft.png";
 import { Row, Col } from "react-bootstrap";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 function Projects() {
   return (
@@ -9,9 +10,14 @@ function Projects() {
       <div className="d-flex justify-content-center pr">
         <Row>
           <Col className="me-5">
+            <p className="d-block text-center pn">Learnhall Official Website</p>
             <img src={LH} alt="" className="proj mx-auto" />
           </Col>
           <Col className="ms-5">
+            <a href="https://nft.whynotswitch.com/" className="text-center pn">
+              Switch NFT Marketplace
+              <LaunchIcon />
+            </a>
             <img src={SW} alt="" className="proj mx-auto" />
           </Col>
         </Row>
